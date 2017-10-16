3f8a374b3090999f5c588e41620d1e53779e9556
Vasyl Kozhushko <kozhushko.vasyl@gmail.com>
2017-10-16 15:58:35 +0100

    fixed link url

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ function go_to_game() {
     document.querySelector(".play_area").style.display = "block"
 }
 start_button.addEventListener("click", go_to_game)
+
+
+// Remove cow
+function remove_cow(cow) {
+    if (parseInt(cow.style.top) >= 500) {
+        $(".hitted").delay(200).show().fadeOut('slow')
+        $(".hitted").removeClass("hitted")
+    }
+}
