29175f09c4c59652ce1e7b8f633a3d29f6e7dd61
Robbie Wood <githubphagocyte@gmail.com>
2017-10-17 10:54:00 +0100

    Make column choice random when dropping a cow.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 var $start_button = $(".start_button");
 
 function cow_time_release() {
-    create_cow(5, 4000)
+    var column = Math.floor(Math.random() * 6)  // Random number from 0 to 5.
+    create_cow(column, 4000)
 }
 
 function create_cow(column_number, fall_time) {
 
-    horizontal_positions = [0,1,2,3,4,5]
-    horizontal_position = horizontal_positions[column_number]
+    var horizontal_position = 5 + 110 * column_number
 
     var $container_div = $("<div class='cow_target_container absolute'><span class='parachute_target'></span>" +
         "<span class='cow_target'></span></div>")
     $container_div.css('top','-177px')
-    $container_div.css('left', column_number+ 'px')
+    $container_div.css('left', horizontal_position + 'px')
     $(".play_area").append($container_div);
     console.log('fish')
     $container_div.animate({ top: '343px'}, fall_time, function () {
