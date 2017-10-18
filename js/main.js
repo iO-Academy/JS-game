5dae132b6e59fb31907b07b639c88bd25a88aaf2
Robbie Wood <githubphagocyte@gmail.com>
2017-10-18 10:48:47 +0100

    Improve clarity following review feedback.

--- js/main.js
+++ js/main.js
@@ -1,39 +1,41 @@
 var $start_button = $(".start_button");
 var fall_time = 4000
-var cow_time = 3000
+var interval_between_cows = 3000
 
-function cow_time_release() {
+function game_loop() {
     var column = Math.floor(Math.random() * 6)  // Random number from 0 to 5.
     increase_speed()
     create_cow(column, fall_time)
+    setTimeout(game_loop, interval_between_cows)
 }
 
 function create_cow(column_number, fall_time) {
+    var horizontal_position = 5 + (110 * column_number)
 
-    var horizontal_position = 5 + 110 * column_number
+    var $container_div = $("<div class='cow_target_container absolute'>" +
+        "<span class='parachute_target'></span>" +
+        "<span class='cow_target js_clickable_cow'></span>" +
+        "</div>")
 
-    var $container_div = $("<div class='cow_target_container absolute'><span class='parachute_target'></span>" +
-        "<span class='cow_target js_clickable_cow'></span></div>")
     $container_div.css('top','-177px')
     $container_div.css('left', horizontal_position + 'px')
+
     $(".play_area").append($container_div);
-    $container_div.animate({ top: '343px'}, fall_time, function () {
-    })
-    setTimeout(cow_time_release, cow_time)
+    $container_div.animate({ top: '343px'}, fall_time, function () {})
 }
 
 // Switch between main page to play page
 function go_to_game() {
     $(".game_title").css("display", "none")
     $(".play_area").css("display","block")
-    cow_time_release()
+    game_loop()
 }
 
 //Increase speed
 function increase_speed() {
-    if (cow_time > 500 && fall_time > 1000) {
+    if (interval_between_cows > 500 && fall_time > 1000) {
         fall_time *= 0.9
-        cow_time *= .9
+        interval_between_cows *= .9
     }
 }
 
