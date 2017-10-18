dc0ea18b968336fd6d1bbaae0aa01863f698e005
Ben Long <bennlongg@gmail.com>
2017-10-18 11:23:26 +0100

    added doc blocks

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 var $start_button = $(".start_button");
-var fall_time = 4000
-var cow_time = 3000
+var fall_time = 3500
+var cow_time = 2500
 var timer
+
+/**
+ * iterates core functions
+ */
 
 function cow_time_release() {
     var column = Math.floor(Math.random() * 6)  // Random number from 0 to 5.
@@ -9,8 +13,13 @@ function cow_time_release() {
     create_cow(column, fall_time)
 }
 
-function create_cow(column_number, fall_time) {
+/**
+ * creates new cows
+ * @param column_number number column from 0-5
+ * @param fall_time  number time it takes for cow to fall
+ */
 
+function create_cow(column_number, fall_time) {
     var horizontal_position = 5 + 110 * column_number
 
     var $container_div = $("<div class='cow_target_container absolute'><span class='parachute_target'></span>" +
@@ -18,33 +27,42 @@ function create_cow(column_number, fall_time) {
     $container_div.css('top','-177px')
     $container_div.css('left', horizontal_position + 'px')
     $(".play_area").append($container_div);
-    // console.log('fish')
+
     $container_div.animate({ top: '343px'}, fall_time, dead_cow)
 
-    // if( get_lives() > 2) {
     timer = setTimeout(cow_time_release, cow_time)
-    // }
+
 }
 
-// Switch between main page to play page
+/**
+ * Switch from intro page to playing page
+ */
+
 function go_to_game() {
     document.querySelector(".lives_value").textContent = 5
     document.querySelector(".score_value").textContent = 0
-    cow_time = 3000
-    fall_time = 4000
+    cow_time = 2500
+    fall_time = 3500
     $(".game_title").css("display", "none")
     $(".play_area").css("display","block")
     cow_time_release()
 }
 
-//Increase speed
+/**
+ * Reduces interval for cows being created and increases speed at which they fall
+ */
 
 function increase_speed() {
-    if (cow_time > 800 && fall_time > 400) {
-        fall_time -= 100
-        cow_time -= 100
+    if (cow_time > 600 && fall_time > 350) {
+        fall_time *= 0.95
+        cow_time *= 0.9
     }
 }
+
+
+/**
+ * Removed cows when they hit the spikes, make them fade out and change image. Also minus a life.
+ */
 
 function dead_cow() {
     $('.cow_target', this).removeClass('js_clickable_cow')
