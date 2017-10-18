91a20c9489d2667ed7453c9c278165d1c7e0ca0e
Dan Tucker <furymuso@aim.com>
2017-10-18 16:51:41 +0100

    midway through merging stashed changes

--- js/page_navigation.js
+++ js/page_navigation.js
@@ -1,60 +1,60 @@
 /**
+ * hides all screen pages then displays the page specified by screen_name
+ *
+ * @param screen_name the selector of the screen you want to be displayed
+ */
+function display_screen(screen_name) {
+    $('.js_screen').css("display", "none")
+    $(screen_name).css("display", "block")
+}
+
+/**
  * initiate_game switches from the game_title page (start) to the play_area page
- * It adds event listeners for the pause button and the keypress for pause (p)
+ * It adds event listeners for the pause keypress of [P]
  */
 function initiate_game() {
-    $(".game_title").css("display", "none")
-    $(".game_pause").css("display", "none")
-    $(".play_area").css("display","block")
+    display_screen('.js_play_area')
 
     $('body').off('keypress')
 
     $('body').on('keypress', function (e) {
         var code = e.keyCode || e.which
-        // p is keycode 112
+        // [P] is keycode 112
         if(code == 112) {
             pause_game()
         }
     })
-
-    $('.pause_button').on('click', pause_game)
-    $('.resume_button').on('click', resume_game)
 }
-
 
 /**
  * pause_game switches from the play_area page to the game_pause page
- * It adds event listeners for the resume button and the keypress for resume (r) and removes any other keypress listeners
+ * It adds event listeners for the keypress for resume [R] and removes any other keypress listeners
  */
 function pause_game() {
-    $(".game_title").css("display", "none")
-    $(".game_pause").css("display", "block")
-    $(".play_area").css("display","none")
-    clearTimeout(timeout_id)
-    $('.cow_target_container').each(function (){
-        $(this).pause()
-    })
+    display_screen('.js_game_pause')
 
     $('body').off('keypress')
 
     $('body').on('keypress', function (e) {
         var code = e.keyCode || e.which
-        // r is keycode 114
+        // [R] is keycode 114
         if(code == 114) {
             resume_game()
         }
     })
+    clearTimeout(timeout_id)
+    $('.cow_target_container').each(function (){
+        $(this).pause()
+    })
 }
-
 
 /**
  * resume_game switches from the game_pause page to the play_area page
  * It adds event listeners for the pause button and the keypress for resume (p) and removes any other keypress listeners
  */
 function resume_game() {
-    $(".game_title").css("display", "none")
-    $(".game_pause").css("display", "none")
-    $(".play_area").css("display","block")
+    display_screen('.js_play_area')
+
     cow_time_release ()
     $('.cow_target_container').each(function (){
         $(this).resume()
@@ -72,11 +72,25 @@ function resume_game() {
     })
 }
 
+function display_instructions() {
+    $(".js_instructions").css("display", "block")
+
+    $('body').off('keypress')
+
+    $('.close_instructions_button').on('click', close_instructions)
+}
+
+function close_instructions() {
+    $(".js_instructions").css("display", "none")
+}
+
 //Shows game_title page and adds initial event listener to initiate the game
-$(".game_title").css("display", "block")
-$(".game_pause").css("display", "none")
-$(".play_area").css("display","none")
-$('.start_button').click(initiate_game)
+display_screen('.js_game_title')
+
+$('.start_button').on('click', initiate_game)
+$('.pause_button').on('click', pause_game)
+$('.resume_button').on('click', resume_game)
+$('.instructions_button').on('click', display_instructions)
 
 
 
